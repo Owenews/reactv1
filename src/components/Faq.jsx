485db79075e671601bed01f75f2f7f0b3eca0733
Owen <owenouiluobe@gmail.com>
2024-12-03 10:19:43 +0100

    file done

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,29 +1,33 @@
 import { useState } from 'react';
 
 const Faq = () => {
-    //Création d'une variable state pour stocker la réponse
-    const [isAnswerDisplayed, setIsAnswerDisplayed] = useState();
+  //Création d'une variable state pour stocker la réponse
+  // Elle nous donne aussi la possibilité d'initialiser la variable
+  const [isAnswerDisplayed, setIsAnswerDisplayed] = useState();
 
-    const displayAnswer = () => {
-      // Modification de la variable isAnswerDisplayed lors du clic
-      setIsAnswerDisplayed (true);
-    };
-
-
+  const displayAnswer = () => {
+    // Modification de la variable isAnswerDisplayed lors du clic à true
+    setTimeout(setIsAnswerDisplayed, 5000, true);
+  };
+    
   return (
     <section>
       <h2>FAQ</h2>
-      <p>What is react ?</p>
-
+      <h3><strong>What is react ?</strong></h3>
+  
         <div>
+            {/*Fonction : lors du clic sur le bouton :
+                Affichage de la réponse
+                Sinon,  affichage du bouton
+            */ }
             {isAnswerDisplayed ? (
                 <p>React is a JavaScript library to build user interfaces.</p>
-                ) : (
-                    <button onClick={displayAnswer}>Wanna know the answer?</button>
+            ) : (
+                <button onClick={displayAnswer}>Wanna know the answer?</button>
                 )}
         </div>
     </section>
   );
-};
+};   
 
 export default Faq;
